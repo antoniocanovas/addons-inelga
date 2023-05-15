f0e75f661a86028eabeceb67f6e66cc29c78d050
antoniolopez9891 <antonio.lopez@serincloud.com>
2023-05-15 17:14:14 +0200

    Modulo

---
+++ parte_trabajo_inelga/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Parte de trabajo inelga",
+    "summary": "Work Part Inelga",
+    "version": "14.0.0.1.0",
+    "category": "Work Part",
+    "author": "SerinCloud, ",
+    "website": "https://ingeniriacloud.com",
+    "license": "AGPL-3",
+    "depends": ['account',
+
+                ],
+    "data": [
+        "views/work_sheet_report_document_report.xml",
+    ],
+    "installable": True,
+}
---
+++ reports_inelga/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": " Factura inelga",
+    "summary": "Reports Inelga",
+    "version": "14.0.0.1.0",
+    "category": "Reports",
+    "author": "SerinCloud, ",
+    "website": "https://ingeniriacloud.com",
+    "license": "AGPL-3",
+    "depends": ['account',
+
+                ],
+    "data": [
+        "views/work_sheet_report_document_report.xml",
+    ],
+    "installable": True,
+}
